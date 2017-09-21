3993ff1f13494e8afabe9b92648bd362b3db539b
weimi <weimi@weimideMac-mini.local>
2017-09-21 17:22:00 +0800

    test selector

--- DValue/ViewController.swift
+++ DValue/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+let kScreenWidth = UIScreen.main.bounds.size.width
+let kGrayColor = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0)
+
 class ViewController: UIViewController {
     
     let carPrice1 = UITextField()
@@ -17,6 +20,7 @@ class ViewController: UIViewController {
     var resultStr : String = String()
     
     let resultLbl = UILabel()
+    let sureBtn = UIButton()
     
     
     override func viewDidLoad() {
@@ -29,54 +33,83 @@ class ViewController: UIViewController {
     func createSubviews() -> Void {
         let label1 = UILabel()
         label1.text = "车1的总价";
-        label1.textColor = UIColor.lightGray
+        label1.textColor = UIColor.black
         label1.frame = CGRect(x: 10, y: 50, width:100, height: 44)
         self.view.addSubview(label1)
         
-        carPrice1.frame = CGRect(x: 150, y: 50, width: 150, height: 44)
+        carPrice1.frame = CGRect(x: kScreenWidth - 150 - 10, y: 50, width: 150, height: 44)
         carPrice1.placeholder = "总价";
+        carPrice1.backgroundColor = kGrayColor
         carPrice1.keyboardType = .decimalPad
+        carPrice1.textAlignment = .center
         self.view.addSubview(carPrice1)
         
         let label2 = UILabel()
         label2.text = "车2的总价";
-        label2.textColor = UIColor.lightGray
+        label2.textColor = UIColor.black
         label2.frame = CGRect(x: 10, y: 100, width:100, height: 44)
         self.view.addSubview(label2)
         
-        carPrice2.frame = CGRect(x: 150, y: 100, width: 150, height: 44)
+        carPrice2.frame = CGRect(x: kScreenWidth - 150 - 10, y: 100, width: 150, height: 44)
         carPrice2.placeholder = "总价";
+        carPrice2.textAlignment = .center
+        carPrice2.backgroundColor = kGrayColor
         carPrice2.keyboardType = .decimalPad
         self.view.addSubview(carPrice2)
         
         let label3 = UILabel()
         label3.text = "车1的百公里油耗";
-        label3.textColor = UIColor.lightGray
+        label3.textColor = UIColor.black
         label3.frame = CGRect(x: 10, y: 150, width:150, height: 44)
         self.view.addSubview(label3)
         
-        gasAmount1.frame = CGRect(x: 150, y: 150, width: 150, height: 44)
+        gasAmount1.frame = CGRect(x: kScreenWidth - 150 - 10, y: 150, width: 150, height: 44)
         gasAmount1.placeholder = "油耗";
+        gasAmount1.textAlignment = .center
+        gasAmount1.backgroundColor = kGrayColor
         gasAmount1.keyboardType = .decimalPad
         self.view.addSubview(gasAmount1)
         
         let label4 = UILabel()
         label4.text = "车2的百公里油耗";
-        label4.textColor = UIColor.lightGray
+        label4.textColor = UIColor.black
         label4.frame = CGRect(x: 10, y: 200, width:150, height: 44)
         self.view.addSubview(label4)
         
-        gasAmount2.frame = CGRect(x: 150, y: 200, width: 150, height: 44)
+        gasAmount2.frame = CGRect(x: kScreenWidth - 150 - 10, y: 200, width: 150, height: 44)
         gasAmount2.placeholder = "油耗";
+        gasAmount2.textAlignment = .center
+        gasAmount2.backgroundColor = kGrayColor
         gasAmount2.keyboardType = .decimalPad
         self.view.addSubview(gasAmount2)
         
-        resultLbl.frame = CGRect(x: 10, y: 250, width: 300, height: 100)
+        resultLbl.frame = CGRect(x: 10, y: 250, width: kScreenWidth - 20, height: 100)
         resultLbl.numberOfLines = 0
+        resultLbl.textColor = UIColor(red: 139/255.0, green: 180/255.0, blue: 78/255.0, alpha: 1.0)
         self.view.addSubview(resultLbl)
+        
+        //MARK:  BUTTON
+        sureBtn.frame = CGRect(x: 15, y: 350, width: kScreenWidth - 30, height: 44)
+        sureBtn.setTitle("计算", for: .normal)
+        sureBtn.setTitleColor(.white, for: .normal)
+        sureBtn.backgroundColor = UIColor(red: 0/255.0, green: 165/255.0, blue: 195/255.0, alpha: 1.0)
+        sureBtn.layer.cornerRadius = 5
+        sureBtn.layer.masksToBounds = true
+        sureBtn.addTarget(self, action: #selector(buttonClick), for: .touchUpInside)
+        self.view.addSubview(sureBtn)
         
     }
     
+    
+    @objc func buttonClick() -> Void {
+        NSLog("button Click")
+        self.textfieldResigning()
+        
+        if !carPrice1.text!.isEmpty && !carPrice2.text!.isEmpty && !gasAmount1.text!.isEmpty && !gasAmount2.text!.isEmpty {
+            self.calculate()
+        }
+    }
+
     /**
       *     计算临界值
       */
@@ -98,16 +131,16 @@ class ViewController: UIViewController {
         
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    func textfieldResigning() -> Void {
         carPrice1.resignFirstResponder()
         carPrice2.resignFirstResponder()
         gasAmount1.resignFirstResponder()
         gasAmount2.resignFirstResponder()
-        
-        if !carPrice1.text!.isEmpty && !carPrice2.text!.isEmpty && !gasAmount1.text!.isEmpty && !gasAmount2.text!.isEmpty {
-                self.calculate()
-        }
-        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //点击空白处失去第一响应
+        self.textfieldResigning()
     }
 
     override func didReceiveMemoryWarning() {
